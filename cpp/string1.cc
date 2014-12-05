4d5587d5942718fafb5dadc3a7a59e50f5af2ada
jiangyx <jiangyx@ctrip.com>
2014-12-05 10:02:33 +0800

    c++ string test

---
+++ cpp/string1.cc
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+//using gcc will cause link error
+//g++ string1.cc -o string1
+
+
+int main()
+{
+  string str("hello world");
+  cout << str << endl;
+}
