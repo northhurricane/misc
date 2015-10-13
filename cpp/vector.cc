adbfb9ab55aeaf32d12c1733bc734fbdbfa12717
jiangyx <jiangyx@ctrip.com>
2015-10-13 16:54:09 +0800

    n/a

---
+++ cpp/vector.cc
@@ -0,0 +1,47 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int insert();
+
+int main(int argc, char *argv[])
+{
+  insert();
+}
+
+int insert( )
+{
+
+  vector <int> v1;
+  vector <int>::iterator Iter;
+
+  v1.push_back( 10 );
+  v1.push_back( 20 );
+  v1.push_back( 30 );
+
+  cout << "v1 =" ;
+  for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ )
+    cout << " " << *Iter;
+  cout << endl;
+
+  v1.insert( v1.begin( ) + 1, 40 );
+  cout << "v1 =";
+  for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ )
+    cout << " " << *Iter;
+  cout << endl;
+
+  v1.insert( v1.begin( ) + 2, 4, 50 );
+  cout << "v1 =";
+  for ( Iter = v1.begin( ) ; Iter != v1.end( ) ; Iter++ )
+    cout << " " << *Iter;
+  cout << endl;
+
+
+
+  v1.insert( v1.begin( )+1, v1.begin( )+2, v1.begin( )+4 );
+  cout << "v1 =";
+  for (Iter = v1.begin( ); Iter != v1.end( ); Iter++ )
+    cout << " " << *Iter;
+  cout << endl;
+}
