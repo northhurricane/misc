aab46d934fa32e03e5a56d9de4787f04f0471cc1
jiangyx <jiangyx@ctrip.com>
2015-11-27 14:34:47 +0800

    n/a

--- ftrace/ftrc.cc
+++ ftrace/ftrc.cc
@@ -1,11 +1,45 @@
 #include "ftrc.h"
+#include <stdint.h>
+#include <string.h>
 
 FTrace::FTrace(const char *name)
 {
   //to : name写入log。
+  int name_len = 0;
+
+  name_len = strlen(name);
+  if (name_len >= MAX_NAME_LEN)
+  {
+    strncpy(name_inner, name, name_len - 1);
+    name_inner[MAX_NAME_LEN - 1] = END_CHAR;
+  }
+  else
+  {
+    strcpy(name_inner, name);
+  }
+
+  char buff[BUFFER_SIZE];
+  buff[0] = END_CHAR;
+
+  strcat(buff, "<");
+  strcat(buff, name_inner);
+  strcat(buff, ">");
+  WriteLog(buff);
 }
 
 FTrace::~FTrace()
 {
   //to : name写入log。
+  char buff[BUFFER_SIZE];
+  buff[0] = END_CHAR;
+
+  strcat(buff, "</");
+  strcat(buff, name_inner);
+  strcat(buff, ">");
+  WriteLog(buff);
+}
+
+void FTrace::WriteLog(char *)
+{
+  return;
 }
--- ftrace/ftrc.h
+++ ftrace/ftrc.h
@@ -1,6 +1,9 @@
 #ifndef _FTRC_H_
 #define _FTRC_H_
 
+#define MAX_NAME_LEN  (256)
+#define BUFFER_SIZE (512)
+#define END_CHAR (0)
 
 #define FUNCTION_TRACE(name) FTrace(name)
 
@@ -14,6 +17,10 @@ class FTrace
 public :
   FTrace(const char *func_name);
   ~FTrace();
+
+private :
+  char name_inner[MAX_NAME_LEN];
+  void WriteLog(char *inf);
 };
 
 #endif //_FTRC_H_
