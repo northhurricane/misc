a3127dd1f1e4f7a445e8f318a1710a1de1d82e3c
jiangyx <jiangyx@ctrip.com>
2016-02-16 16:14:15 +0800

    na

--- cpp/map.cc
+++ cpp/map.cc
@@ -1,5 +1,6 @@
 #include <map>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -12,18 +13,21 @@ int main(int argc, char *argv[])
   //插入数据
   name_age_map["mary"] = 23;
   name_age_map["john"] = 32;
-  string name = "jack";
+  string name = "john";
   int age = 0;
   //查找数据
   string_int_map_it_t it = name_age_map.find(name);
   if (it == name_age_map.end())
   {
     //not found
+    cout << "not found" << endl;
   }
   else
   {
     //found
     age = it->second;
+    cout << "found!!";
+    cout << "name : " << it->first << ", age : " << it->second << "." << endl;
     //删除数据
     name_age_map.erase(it);
   }
