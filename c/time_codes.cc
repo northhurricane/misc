6856e7bcf2f70ede3262ae5b3d4e9c6698d19c2c
姜宇祥 <jiangyuxiang@hotpu.cn>
2019-04-29 16:35:37 +0800

    posix计时示例

--- c/time_codes.cc
+++ c/time_codes.cc
@@ -27,9 +27,25 @@ c99_time_diff()
   printf("interval between %d.\n", (int)difftime(t2, t1));
 }
 
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <stdint.h>
+void
+posix_timer()
+{
+  struct timespec time_start={0, 0},time_end={0, 0};
+  clock_gettime(CLOCK_REALTIME, &time_start);
+  sleep(1);
+  clock_gettime(CLOCK_REALTIME, &time_end);
+  uint64_t duration = (time_end.tv_sec - time_start.tv_sec) * 1000000000
+                      + (time_end.tv_nsec-time_start.tv_nsec);
+  std::cout << "duration is " << duration << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-  c99_time_diff();
+  //c99_time_diff();
+  posix_timer();
   return 0;
 }
