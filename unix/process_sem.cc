d2f3c90e5d0332b9030012b6a67004443be5a2dd
姜宇祥 <jiangyuxiang@hotpu.cn>
2019-07-17 10:47:22 +0800

    process semaphore

---
+++ unix/process_sem.cc
@@ -0,0 +1,97 @@
+/*
+g++ process_sem.cc -o process_sem -lpthread -g -O0
+sem_unlink 从系统中将该名字semaphore移除
+*/
+
+#include <semaphore.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define SEM_NAME   "my_sem"
+
+static void process_producer()
+{
+  cout << "enter producer process" << endl;
+  sem_t *sem = NULL;
+  sem = sem_open(SEM_NAME, O_CREAT|O_RDWR, 0666, 0);
+  if (sem == NULL)
+  {
+    cout << "fail open sem." << endl;
+    exit(-1);
+  }
+
+  string cmd;
+  int r = 0;
+  while (true)
+  {
+    cout << "please input message." << endl;
+    cin >> cmd;
+    if (cmd == "q")
+      break;
+    cout << "sem post" << endl;
+    r = sem_post(sem);
+    if (r < 0)
+      cout << "fail post" << endl;
+  }
+  r = sem_unlink(SEM_NAME);
+  if (r < 0)
+    cout << "failed unlink." << endl;
+  r = sem_close(sem);
+  if (r < 0)
+    cout << "failed close." << endl;
+
+  exit(0);
+}
+
+static void process_consumer()
+{
+  cout << "enter consumer process" << endl;
+
+  sem_t *sem = NULL;
+  sem = sem_open(SEM_NAME, O_CREAT|O_RDWR, 0666, 0);
+  if (sem == NULL)
+  {
+    cout << "fail open sem." << endl;
+    exit(-1);
+  }
+
+  string cmd;
+  int r = 0;
+  while (true)
+  {
+    cout << "please input message." << endl;
+    cin >> cmd;
+    if (cmd == "q")
+      break;
+    cout << "sem wait" << endl;
+    r = sem_wait(sem);
+  }
+  r = sem_unlink(SEM_NAME);
+  if (r < 0)
+    cout << "failed unlink." << endl;
+  r = sem_close(sem);
+  if (r < 0)
+    cout << "failed close." << endl;
+
+  exit(0);
+}
+
+int main(int argc, const char *argv[])
+{
+  if (argc == 1)
+  {
+    process_producer();
+  }
+  else
+  {
+    process_consumer();
+  }
+
+  return 0;
+}
