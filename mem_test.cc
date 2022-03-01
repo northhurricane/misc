29867d173eedfc2d58b66fb3599d3541dc8d54f4
yuxianjiang <yuxianjiang@tencent.com>
2022-03-01 16:36:12 +0800

    test for memory alloc occupy
    Signed-off-by: yuxianjiang <yuxianjiang@tencent.com>

---
+++ mem_test.cc
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <sys/mman.h>
+
+static int stop_thread = 0;
+
+int sleep_in_second(int n)
+{
+  struct timespec time_to_sleep, time_real_sleep;
+
+  time_to_sleep.tv_sec = n;
+  time_to_sleep.tv_nsec = 0;
+  nanosleep(&time_to_sleep, &time_real_sleep);
+}
+
+static void* thread_func(void *arg)
+{
+  int temp = (int)((long long)arg);
+  printf("thread %d\n", temp);
+  if (temp == 1)
+  {
+    //thread 1
+    //local variables memory test
+    const int local_size = 4*1024*1024;
+    char buf[local_size];
+    memset(buf, 0, local_size);
+
+    //malloc memory test
+    const int malloc_size(8*1024*1024);
+    char *p = (char*)malloc(malloc_size);
+    for (int i = 0; i < malloc_size; i++)
+    {
+      p[i] = (i % 26) + 'a';
+    }
+
+    //mmap memory test;
+    const int mmap_size = 16*1024*1024;
+    char *p2 = nullptr;
+    p2 = (char*)mmap(NULL, mmap_size, PROT_READ | PROT_WRITE,
+         MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    for (int i = 0; i < mmap_size; i++)
+    {
+      p2[i] = (i % 26) + 'a';
+    }
+  }
+  else
+  {
+    //thread 2
+  }
+
+  while (1)
+  {
+    if (stop_thread)
+      break;
+    sleep_in_second(1);
+  }
+  return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+  pthread_t thread;
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  const int stack_16m = 16*1024*1024;
+  pthread_attr_setstacksize(&attr, stack_16m);
+
+  //default 8M thread stack size
+  pthread_create(&thread,&attr,&thread_func,(void*)1);
+  //16M thread stack size
+  pthread_create(&thread,NULL,&thread_func,(void*)2);
+
+  getchar();
+  stop_thread = 1;
+
+  return 0;
+}
+//g++ mem_test.cc  -o mem_test -lrt -lpthread -std=c++11
+
